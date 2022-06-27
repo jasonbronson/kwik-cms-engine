0a9496f47905da949ac62f95566fb3e1449ca54f
Vien Nguyen <viennguyendeveloper@gmail.com>
2022-06-27 08:35:24 +0700

    Update

--- route/routes.go
+++ route/routes.go
@@ -83,15 +83,16 @@ func Router(newRelicApp *newrelic.Application) http.Handler {
 			posts.GET("", request.GetPosts)
 			posts.GET("/:postid", request.GetPost)
 			posts.POST("", request.SetPosts)
-			posts.PUT("/publish/:postid", request.UpdatePublishDate)
 			posts.PUT("/:postid", request.PutPosts)
 			posts.DELETE("/:postid", request.DeletePosts)
+			posts.PUT("/publish/:id", request.UpdatePublishDate)
 		}
 		pages := api.Group("/pages")
 		{
 			pages.GET("", request.GetPages)
 			pages.GET("/:pageid", request.GetPage)
 			pages.POST("", request.SetPages)
+			pages.PUT("/publish/:id", request.UpdatePublishDate)
 			pages.PUT("/:pageid", request.PutPages)
 			pages.DELETE("/:pageid", request.DeletePages)
 		}
